a0e20e3273f9f8648ad31c00fed097f1f410cbb6
an0i <1196557571@qq.com>
2023-11-02 22:48:34 +0800

    Alpha

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,35 @@
 <script>
   import LinePlot from "./LinePlot.svelte";
 
+  let tstr = "30 40 50 60 65 70 75 80 85";
   let u1str = "0.1127 0.1166 0.1209 0.1250 0.1270 0.1290 0.1308 0.1329 0.1348";
   let u2str = "537.6 500.3 477.1 452.3 441.9 428.3 415.1 402.2 390.9";
   let u3str = "317.9 455.9 570.1 678.1 722.4 779.9 835.3 890.4 938.4";
 
+  $: t = tstr
+    .trim()
+    .split(/\s+/)
+    .map((s) => parseFloat(s) || 0);
   $: u1 = u1str
     .trim()
-    .split(" ")
-    .map((s) => parseFloat(s));
+    .split(/\s+/)
+    .map((s) => parseFloat(s) || 0)
+    .concat(Array(t.length).fill(0))
+    .slice(0, t.length);
   $: u2 = u2str
     .trim()
-    .split(" ")
-    .map((s) => parseFloat(s));
+    .split(/\s+/)
+    .map((s) => parseFloat(s) || 0)
+    .concat(Array(t.length).fill(0))
+    .slice(0, t.length);
   $: u3 = u3str
     .trim()
-    .split(" ")
-    .map((s) => parseFloat(s));
+    .split(/\s+/)
+    .map((s) => parseFloat(s) || 0)
+    .concat(Array(t.length).fill(0))
+    .slice(0, t.length);
+
+  let plot;
 </script>
 
 <div class="container mx-auto flex min-h-screen flex-col gap-4 p-4">
@@ -35,7 +48,22 @@
   <div>
     <input bind:value={u3str} placeholder="LM35 (mV)" class="w-full rounded border border-gray-300 p-3 hover:shadow focus:outline-gray-600" type="text" />
   </div>
-  <div class="hover:shadow rounded-xl border w-fit overflow-hidden">
-    <LinePlot {u1} {u2} {u3} />
+  <div>
+    <p class="w-fit rounded bg-gray-100 px-3 py-2 text-sky-950">写完网站才发现svg不好打印，白写了一天，实在要打印的话试试 Inkscape 吧</p>
+  </div>
+  <div bind:this={plot} class="hover:shadow rounded-xl border w-fit overflow-hidden">
+    <button
+      on:click={() => {
+        const blobstr = new XMLSerializer().serializeToString(plot.getElementsByTagName("svg")[0]);
+        const blob = new Blob([blobstr], { type: "image/svg+xml" });
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "result.svg";
+        link.click();
+      }}
+      class="disabled:bg-gray-100 rounded-br-xl border-r border-b border-gray-300 px-4 py-1.5 hover:bg-gray-50 hover:shadow hover:active:bg-gray-100"
+      type="button">下载 SVG</button
+    >
+    <LinePlot {t} {u1} {u2} {u3} />
   </div>
 </div>
--- src/LinePlot.svelte
+++ src/LinePlot.svelte
@@ -1,7 +1,8 @@
 <script>
   import * as d3 from "d3";
 
-  export let t = [30, 40, 50, 60, 65, 70, 75, 80, 85];
+  /** @type number[] */
+  export let t;
   /** @type number[] */
   export let u1;
   /** @type number[] */
@@ -19,12 +20,12 @@
   $: y3 = d3.scaleLinear(d3.extent(u3), [sectionHeight * 3 + gap * 3, sectionHeight * 2 + gap * 3]);
 
   let gx1, gx2, gx3, gy1, gy2, gy3;
-  $: d3.select(gx1).call(d3.axisBottom(x));
-  $: d3.select(gx2).call(d3.axisBottom(x));
-  $: d3.select(gx3).call(d3.axisBottom(x));
-  $: d3.select(gy1).call(d3.axisLeft(y1));
-  $: d3.select(gy3).call(d3.axisLeft(y3));
-  $: d3.select(gy2).call(d3.axisLeft(y2));
+  $: console.log(d3.axisBottom(x));
+  $: d3.axisBottom(x);
+  $: d3.axisBottom(x);
+  $: d3.axisLeft(y1);
+  $: d3.axisLeft(y3);
+  $: d3.axisLeft(y2);
 
   /**
    * @param {number[]} xArray
@@ -76,6 +77,21 @@
   <g bind:this={gy1} transform="translate({gap},0)" />
   <g bind:this={gy2} transform="translate({gap},0)" />
   <g bind:this={gy3} transform="translate({gap},0)" />
+  <text x={(width - gap) / 2} y={gap * 1 + sectionHeight * 0}>Pt100</text>
+  <text x={(width - gap) / 2} y={gap * 2 + sectionHeight * 1}>PN</text>
+  <text x={(width - gap) / 2} y={gap * 3 + sectionHeight * 2}>LM35</text>
+  <text x={width - gap} y={gap * 1 + sectionHeight * 1 + 30} font-size="0.75rem">t (°C)</text>
+  <text x={width - gap} y={gap * 2 + sectionHeight * 2 + 30} font-size="0.75rem">t (°C)</text>
+  <text x={width - gap} y={gap * 3 + sectionHeight * 3 + 30} font-size="0.75rem">t (°C)</text>
+  <text x={gap / 3} y={gap * 1 + sectionHeight * 0 - 10} font-size="0.75rem">U (V)</text>
+  <text x={gap / 3} y={gap * 2 + sectionHeight * 1 - 10} font-size="0.75rem">U (mV)</text>
+  <text x={gap / 3} y={gap * 3 + sectionHeight * 2 - 10} font-size="0.75rem">U (mV)</text>
+  <text x={(width - gap) / 2} y={gap * 1 + sectionHeight * 0.5 + 30} font-size="0.75rem">y={l1.k}x{l1.b > 0 ? "+" + l1.b : "" + l1.b}</text>
+  <text x={(width - gap) / 2} y={gap * 1 + sectionHeight * 0.5 + 50} font-size="0.75rem">r={l1.r}</text>
+  <text x={(width - gap) / 2} y={gap * 2 + sectionHeight * 1.5 - 30} font-size="0.75rem">y={l2.k}x{l2.b > 0 ? "+" + l2.b : "" + l2.b}</text>
+  <text x={(width - gap) / 2} y={gap * 2 + sectionHeight * 1.5 - 50} font-size="0.75rem">r={l2.r}</text>
+  <text x={(width - gap) / 2} y={gap * 3 + sectionHeight * 2.5 + 30} font-size="0.75rem">y={l3.k}x{l3.b > 0 ? "+" + l3.b : "" + l3.b}</text>
+  <text x={(width - gap) / 2} y={gap * 3 + sectionHeight * 2.5 + 50} font-size="0.75rem">r={l3.r}</text>
   <g fill="white" stroke="currentColor" stroke-width="1.5">
     {#each u1 as d, i}
       <circle cx={x(t[i])} cy={y1(d)} r="2.5" />
